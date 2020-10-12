fa6d51e94902950b453f8bbb73170e818e4302eb
Jose_Martinez <al0277334@tecmilenio.mx>
2020-10-12 16:53:30 -0500

    First Commit

---
+++ src/com/ORMUZ09a/Tecmilenio/Game.java
@@ -0,0 +1,13 @@
+package com.ORMUZ09a.Tecmilenio;
+
+public class Game {
+
+    public class Deck{
+        
+
+    }
+    public class Card{
+
+
+    }
+}
---
+++ src/com/ORMUZ09a/Tecmilenio/Main.java
@@ -0,0 +1,8 @@
+package com.ORMUZ09a.Tecmilenio;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
