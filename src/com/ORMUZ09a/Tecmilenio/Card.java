6e14b4b594ce4df67853fc2dc471047e1a2fa126
Jose_Martinez <al0277334@tecmilenio.mx>
2020-10-12 20:50:23 -0500

    Changing Gaming

---
+++ src/com/ORMUZ09a/Tecmilenio/Card.java
@@ -0,0 +1,9 @@
+package com.ORMUZ09a.Tecmilenio;
+
+public class Card {
+        private int numero;
+        private String palo;
+        private String color;
+        public static final String[] PALOS= ("Picas", "Corazones", "Diamantes", "Treboles");
+}
+
---
+++ src/com/ORMUZ09a/Tecmilenio/Deck.java
@@ -0,0 +1,11 @@
+package com.ORMUZ09a.Tecmilenio;
+
+public class Deck {
+
+
+    public void shuffle{
+        
+
+    }
+
+}
